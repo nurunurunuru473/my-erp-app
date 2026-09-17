b2750b1cb416167b9fbd0b377303c0f455611bb1
nurunurunuru473 <Jaduhaj3@gmail.com>
2026-09-17 23:39:25 +0300

    Add Vercel entrypoint in api folder

---
+++ api/index.mjs
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '..')));
+
+app.get('/api/test', (req, res) => {
+  res.json({ message: "Server is running perfectly!" });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
+
+export default app;
